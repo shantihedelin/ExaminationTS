28772f8d1106698d6c180178b9636262a4af05ca
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-11-11 17:57:10 +0100

    Fixed font

--- project/app/layout.tsx
+++ project/app/layout.tsx
@@ -1,18 +1,24 @@
-"use client"
+"use client";
 import localFont from "next/font/local";
+import { Fira_Sans } from "next/font/google";
 import "./globals.css";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+const firaSans = Fira_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export default function RootLayout({
@@ -21,10 +27,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={firaSans.className}>
+      <body>
         <Provider store={store}>{children}</Provider>
       </body>
     </html>
